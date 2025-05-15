23aa59a1fea1d381f01bbfe6fac919aa1d76ede5
Shreshta <99638225+Shreshthashet@users.noreply.github.com>
2025-05-15 22:31:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_login import LoginManager, current_user, login_user, logout_user, log
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_socketio import SocketIO
 from models import User, Child, WebActivity, Alert, OnlineSession
+from extensions import db
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
